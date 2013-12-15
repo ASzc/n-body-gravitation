7a4a24fb5c0c582cf30139065dc716a234125146
Alex Szczuczko <alex@szc.ca>
2013-12-15 15:50:42 -0500

    Remove SwingUtilities.invokeLater use in main: no longer required (NEWT being used).

--- src/ca/szc/physics/nbodygravitation/Main.java
+++ src/ca/szc/physics/nbodygravitation/Main.java
@@ -18,8 +18,6 @@
  */
 package ca.szc.physics.nbodygravitation;
 
-import javax.swing.SwingUtilities;
-
 import ca.szc.physics.nbodygravitation.opengl.OpenGlSetup;
 
 // Created with reference to http://physics.princeton.edu/~fpretori/Nbody
@@ -31,6 +29,6 @@ public class Main
      */
     public static void main( String[] args )
     {
-        SwingUtilities.invokeLater( new OpenGlSetup() );
+        new OpenGlSetup().run();
     }
 }
